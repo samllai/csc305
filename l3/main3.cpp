2a99137fe12ef436f0a113299bfd95d607936e63
Samantha Lai <samllai@shaw.ca>
2020-03-09 16:27:52 -0700

    adding previous lab work

---
+++ l3/main3.cpp
@@ -0,0 +1,256 @@
+#include "lab.hpp"
+#include <iostream>
+
+// ******* Function Member Implementation *******
+
+// ***** Shape function members *****
+Shape::Shape() : mColour{0, 0, 0}
+{}
+
+void Shape::setColour(Colour const& col)
+{
+    mColour = col;
+}
+
+Colour Shape::getColour() const
+{
+    return mColour;
+}
+
+// ***** Camera function members *****
+Camera::Camera() :
+    mEye{0.0f, 0.0f, 500.0f},
+    mLookAt{0.0f},
+    mUp{0.0f, 1.0f, 0.0f},
+    mU{1.0f, 0.0f, 0.0f},
+    mV{0.0f, 1.0f, 0.0f},
+    mW{0.0f, 0.0f, 1.0f}
+{}
+
+void Camera::setEye(atlas::math::Point const& eye)
+{
+    mEye = eye;
+}
+
+void Camera::setLookAt(atlas::math::Point const& lookAt)
+{
+    mLookAt = lookAt;
+}
+
+void Camera::setUpVector(atlas::math::Vector const& up)
+{
+    mUp = up;
+}
+
+void Camera::computeUVW()
+{
+	mW = (mEye - mLookAt) / glm::length(mEye - mLookAt);
+	mU = glm::cross(mUp, mW) / glm::length(glm::cross(mUp, mW));
+	mV = glm::cross(mW, mU);
+}
+
+
+void Pinhole::renderScene(World& world) const {
+	atlas::math::Ray<atlas::math::Vector> ray;
+	ray.o = mEye;
+
+	atlas::math::Point sample_point;
+	atlas::math::Point pixel_point;
+
+	for (int i = 0; i < world.width; i++) {
+		for (int j = 0; j < world.height; j++) {
+			Colour avg_colour{ 0,0,0 };
+			ShadeRec sr;
+			sr.color = { 0,0,0 };
+			sr.t = std::numeric_limits<float>::max();
+
+			for (int s = 0; s < world.sampler->getNumSamples; s++) {
+				sample_point = world.sampler->sampleUnitSquare();
+				pixel_point.x = (i - 0.5f * (world.width - 1.0f));
+				pixel_point.y = (j - 0.5f * (world.height - 1.0f));
+				pixel_point.z = dist;
+				sample_point += pixel_point;
+				ray.d = glm::normalize(sample_point);
+
+				for (auto&& object : world.scene) {
+					object->hit(ray, sr);
+				}
+				if (sr.t = std::numeric_limits<float>::max()) {
+					avg_colour = world.background;
+				}
+				else {
+					avg_colour /= world.sampler->getNumSamples;
+				}
+				world.image.push_back(avg_colour);
+			}
+		}
+	}
+}
+
+// ***** Sampler function members *****
+Sampler::Sampler(int numSamples, int numSets) :
+    mNumSamples{numSamples}, mNumSets{numSets}, mCount{0}, mJump{0}
+{
+    mSamples.reserve(mNumSets * mNumSamples);
+    setupShuffledIndeces();
+}
+
+int Sampler::getNumSamples() const
+{
+    return mNumSamples;
+}
+
+void Sampler::setupShuffledIndeces()
+{
+    mShuffledIndeces.reserve(mNumSamples * mNumSets);
+    std::vector<int> indices;
+
+    std::random_device d;
+    std::mt19937 generator(d());
+
+    for (int j = 0; j < mNumSamples; ++j)
+    {
+        indices.push_back(j);
+    }
+
+    for (int p = 0; p < mNumSets; ++p)
+    {
+        std::shuffle(indices.begin(), indices.end(), generator);
+
+        for (int j = 0; j < mNumSamples; ++j)
+        {
+            mShuffledIndeces.push_back(indices[j]);
+        }
+    }
+}
+
+atlas::math::Point Sampler::sampleUnitSquare()
+{
+    if (mCount % mNumSamples == 0)
+    {
+        atlas::math::Random<int> engine;
+        mJump = (engine.getRandomMax() % mNumSets) * mNumSamples;
+    }
+
+    return mSamples[mJump + mShuffledIndeces[mJump + mCount++ % mNumSamples]];
+}
+
+// ***** Sphere function members *****
+Sphere::Sphere(atlas::math::Point center, float radius) :
+    mCentre{center}, mRadius{radius}, mRadiusSqr{radius * radius}
+{}
+
+bool Sphere::hit(atlas::math::Ray<atlas::math::Vector> const& ray,
+                 ShadeRec& sr) const
+{
+    float t{};
+    bool intersect{intersectRay(ray, t)};
+
+    // update ShadeRec info about new closest hit
+    if (intersect && t < sr.t)
+    {
+        sr.color = mColour;
+        sr.t     = t;
+    }
+
+    return intersect;
+}
+
+bool Sphere::intersectRay(atlas::math::Ray<atlas::math::Vector> const& ray,
+                          float& tMin) const
+{
+    const auto tmp{ray.o - mCentre};
+    const auto a{glm::dot(ray.d, ray.d)};
+    const auto b{2.0f * glm::dot(ray.d, tmp)};
+    const auto c{glm::dot(tmp, tmp) - mRadiusSqr};
+    const auto disc{(b * b) - (4.0f * a * c)};
+
+    if (atlas::core::geq(disc, 0.0f))
+    {
+        const float kEpsilon{0.01f};
+        const float e{std::sqrt(disc)};
+        const float denom{2.0f * a};
+
+        // Look at the negative root first
+        float t = (-b - e) / denom;
+        if (atlas::core::geq(t, kEpsilon))
+        {
+            tMin = t;
+            return true;
+        }
+
+        // Now the positive root
+        t = (-b + e);
+        if (atlas::core::geq(t, kEpsilon))
+        {
+            tMin = t;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// ***** Regular function members *****
+Regular::Regular(int numSamples, int numSets) : Sampler{numSamples, numSets}
+{
+    generateSamples();
+}
+
+void Regular::generateSamples()
+{
+    int n = static_cast<int>(glm::sqrt(static_cast<float>(mNumSamples)));
+
+    for (int j = 0; j < mNumSets; ++j)
+    {
+        for (int p = 0; p < n; ++p)
+        {
+            for (int q = 0; q < n; ++q)
+            {
+                mSamples.push_back(
+                    atlas::math::Point{(q + 0.5f) / n, (p + 0.5f) / n, 0.0f});
+            }
+        }
+    }
+}
+
+// ******* Driver Code *******
+
+int main()
+{
+	//std::cout << "hello" << std::endl;
+
+	Pinhole phcam {1000.0f};
+	World w;
+	w.width = 600;
+	w.height = 600;
+	w.background = { 0,0,0 };
+
+	std::shared_ptr<Regular> sample = std::make_shared<Regular> ( 16, 16 );
+	sample->generateSamples();
+	w.sampler = sample;
+
+	return 0;
+}
+
+void saveToFile(std::string const& filename,
+                std::size_t width,
+                std::size_t height,
+                std::vector<Colour> const& image)
+{
+    std::vector<unsigned char> data(image.size() * 3);
+
+    for (std::size_t i{0}, k{0}; i < image.size(); ++i, k += 3)
+    {
+        Colour pixel = image[i];
+        data[k + 0]  = static_cast<unsigned char>(pixel.r * 255);
+        data[k + 1]  = static_cast<unsigned char>(pixel.g * 255);
+        data[k + 2]  = static_cast<unsigned char>(pixel.b * 255);
+    }
+
+    stbi_write_bmp(filename.c_str(),
+                   static_cast<int>(width),
+                   static_cast<int>(height),
+                   3,
+                   data.data());
+}
